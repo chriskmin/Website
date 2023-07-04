d7e293ba6d35d7d336c35ccf639fa71a2c4a4121
Chris Min <chrismin@Bu.edu>
2023-07-04 14:39:49 -0400

    Adding self-introduction section

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import {FaLinkedin, FaGithub, FaArrowDown} from "react-icons/fa"
+import React from 'react';
+import {FaTwitter, FaLinkedin, FaGithub, FaArrowDown} from "react-icons/fa";
+import img from "../Assets/img.jpg";
 
 const Me = () => {
-  return  <div></div>;
-}
+    const SOCIAL = [
+        {
+            id : 1,
+            link:"https://https://twitter.com/ChrisKMin1",
+            icon: <FaTwitter />,
+        },
+        {
+            id : 2,
+            link:"https://www.linkedin.com/in/christopher-min/",
+            icon: <FaLinkedin/>,
+        },
+        {
+            id : 3,
+            link:"https://github.com/chriskmin",
+            icon: <FaGithub/>,
+        },
+    ];
+  return  <section>
+    <h2>Chris Min</h2>
+    <h3>CS Student @ Boston University</h3>
+    <p>
+        Hello! Welcome to my website! I'm currently a junior at Boston University studying computer science with a minor in business administration. 
+        I'm currently interested in AI/ML and embedded systems so I am working on a couple projects regarding these interests.
+        As for hobbies, I like to play basketball or lift, and so you'll often find me in the gym!
+    </p>
+  </section>;
+};
 
 export default Me
