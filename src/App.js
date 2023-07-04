19fefe50051018cd9f9215779a6f138454f0bf0e
Chris Min <chrismin@Bu.edu>
2023-07-04 13:53:45 -0400

    Initializing dark mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+    const[darkMode, setDarkMode] = useState(false);
+
+
     return(
         <div>
            <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import {MdNightsStay, MdWbSunny} from 'react-icons/md'
 
 const Header = () => {
   return (
-  <header>
-    <nav>
-      <h1> Chris Min</h1>
+  <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+    <nav className="flex justify-between items-center p-5">
+      <h1 className="text-xl">Chris Min</h1>
 
       <div>
-        <MdNightsStay />
-        <MdWbSunny />
+        <MdNightsStay className="text-2xl cursor-pointer"/>
+        <MdWbSunny className="text-2xl cursor-pointer"/>
       </div>
 
     </nav>
