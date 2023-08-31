9d65f14b46b346e4f895fbb73b4cdb9c70415a0e
Chris Min <chrismin@Bu.edu>
2023-08-31 11:13:31 -0400

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return <section className="min-h-fit flex flex-col justify-start items-center py-16 px-5 text-center">  
     <p className="flex max-w-xl font-light items-center justify-center text-gray-500">
-    © 2023 chrismin.com All rights reserved. 
+    © 2023 chriskmin.com All rights reserved. 
     </p>
   </section>
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,11 +2,40 @@ import React from 'react'
 import Section from './common/Section'
 
 const Projects = () => {
+
+  const projects = [
+    {
+      id: 1,
+      image: p1,
+      title: 'Music App',
+      github: ''
+    },
+    {
+      id: 1,
+      image: p1,
+      title: 'Music App',
+      github: ''
+    },
+    {
+      id: 1,
+      image: p1,
+      title: 'Music App',
+      github: ''
+    },
+
+
+
+  ]
+
   return (
     <Section 
     title="Projects"
-    subtitle="WORK IN PROGRESS, WILL UPDATE AS MORE PROJECTS ARE COMPLETED"
-    > </Section>
+    subtitle="These are the projects that I've worked on"
+    > 
+      <div>
+
+      </div>
+    </Section>
   )
 }
 
